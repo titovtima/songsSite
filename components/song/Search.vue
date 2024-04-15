ec93f16df081c4f34b264c9428dc677fff719033
titovtima <titov.tima.spb@yandex.ru>
2024-04-15 18:21:57 +0300

    extract song search into component

---
+++ components/song/Search.vue
@@ -0,0 +1,38 @@
+<template>
+  <input type="text" placeholder="Поиск песни по словам" ref="searchInput" class="w-full border border-gray-400 p-1">
+</template>
+
+<script setup lang="ts">
+const searchInput: any = ref(null);
+const searchValue = ref('');
+
+const props = defineProps(['searchList']);
+const emits = defineEmits(['displayList']);
+
+onMounted(() => {
+  searchInput.value.oninput = () => {
+    searchValue.value = searchInput.value.value.toLowerCase();
+    if (!searchInput.value || searchValue.value == '')
+      emits('displayList', props.searchList);
+    let searchArr = searchValue.value.split(/[^\p{L}]/gu).filter(w => w.length > 0);
+    emits('displayList', props.searchList.filter((song: any) => {
+      return searchArr.reduce((acc: boolean, word: string) => {
+        if (!acc) return false;
+        if (song.name.toLowerCase().includes(word)) return true;
+        for (let part of song.parts) {
+          if (part.data.toLowerCase().includes(word)) return true;
+        }
+        return false;
+      }, true)
+    }).sort((song1: { name: string; }, song2: { name: string; }) => {
+      if (song1.name < song2.name) return -1;
+      if (song1.name > song2.name) return 1;
+      else return 0;
+    }));
+  }
+});
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <input type="text" placeholder="Поиск песни по словам" ref="searchInput" class="w-full border border-gray-400 p-1">
+    <SongSearch :search-list="toValue(mainListData)" @display-list="(list) => searchedList = list"/>
     <SongList :list="toValue(displayList)" class="mt-5"/>
   </div>
 </template>
@@ -11,31 +11,13 @@ import {toValue} from "vue";
 
 useHead({title: 'Песни церкви «Источник жизни»'});
 
-const searchInput: any = ref(null);
-const searchValue = ref('');
-onMounted(() => {
-  searchInput.value.oninput = () => {
-    searchValue.value = searchInput.value.value.toLowerCase();
-  }
-});
-
 const mainListInfo = ref([]);
 const mainListData = ref([]);
+const searchedList: any = ref(null);
 const displayList = computed(() => {
-  if (!searchInput.value || mainListData.value.length == 0 || searchValue.value == '')
+  if (!mainListData.value || mainListData.value.length == 0 || searchedList.value == null)
     return mainListInfo.value;
-  let searchArr = searchValue.value.split(/[^\p{L}]/gu).filter(w => w.length > 0);
-  console.log(`filtering by `, searchArr);
-  return mainListData.value.filter((song: any) => {
-    return searchArr.reduce((acc: boolean, word: string) => {
-      if (!acc) return false;
-      if (song.name.toLowerCase().includes(word)) return true;
-      for (let part of song.parts) {
-        if (part.data.toLowerCase().includes(word)) return true;
-      }
-      return false;
-    }, true)
-  }).sort((song1: { name: string; }, song2: { name: string; }) => {
+  return searchedList.value.sort((song1: { name: string; }, song2: { name: string; }) => {
     if (song1.name < song2.name) return -1;
     if (song1.name > song2.name) return 1;
     else return 0;
