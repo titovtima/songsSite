0696b78f74bc6cedd49df958b095a0f3eb05946f
titovtima <titov.tima.spb@yandex.ru>
2024-10-08 16:24:29 +0300

    import components with no prefix path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,24 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/main.css'],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
+  components: [
+    {
+      path: '~/components',
+      pathPrefix: false,
+    },
+  ],
+  
   modules: [
     '@nuxtjs/device',
-  ]
-})
+  ],
+
+  compatibilityDate: '2024-10-08',
+});
