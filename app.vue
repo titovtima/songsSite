9d8b986af4fdf74e92d65ff596a6aaaa6faccb64
titovtima <titov.tima.spb@yandex.ru>
2025-11-15 21:35:33 +0300

    pwa without dependency

--- app.vue
+++ app.vue
@@ -1,45 +1,29 @@
 <template>
   <YandexMetrika />
-  <NuxtPwaManifest />
   <div ref="scrollDiv" class="overflow-y-auto h-full p-[20px] pb-[100px]">
     <Navbar @click-settings="showSettingsModal = true"/>
     <NuxtPage />
-    <div>
-      <button v-if="canInstall" @click="installPWA">Install App</button>
-    </div>  </div>
+  </div>
   <Settings v-model:show="showSettingsModal"/>
 </template>
 
 <script setup lang="ts">
-import { useHead, useNuxtApp } from '#app';
-import { ref, onMounted } from 'vue';
-
 const route = useRoute();
 const scrollDiv: Ref<any> = ref(null);
-const { $pwa } = useNuxtApp();
-const canInstall = ref(false);
 
-useHead({ link: [{href: '/favicon.ico', rel: 'icon'}] });
-watchEffect( () => {
+useHead({ link: [{href: '/logo-180.png', rel: 'icon'}] });
+watchEffect(() => {
   useHead({link: [{href: 'https://songs.istokspb.org' + route.path, rel: 'canonical'}]});
 });
+
+useHead({link: [{rel: 'manifest', href: '/manifest.json'}]})
 
 const showSettingsModal = ref(false);
 
 onMounted(() => {
-  if ($pwa && $pwa.showInstallPrompt) {
-    canInstall.value = $pwa.showInstallPrompt;
-  }
   useState('mainScrollDiv').value = scrollDiv.value;
   watchScroll();
 });
-
-const installApp = () => {
-  if ($pwa && $pwa.install) {
-    $pwa.install();
-    canInstall.value = false; // Скрываем баннер после попытки установки
-  }
-};
 
 function watchScroll() {
   scrollDiv.value.addEventListener('scroll', () => {
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,22 +5,22 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/main.css'],
 
-  app: {
-    head: {
-      meta: [
-        { name: 'description', content: 'Сборник текстов песен для Церкви Источник Жизни' },
-        { name: 'theme-color', content: '#ffffff' },
-        { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
-        { name: 'apple-mobile-web-app-capable', content: 'yes' },
-        { name: 'apple-mobile-web-app-title', content: 'Песни ИЖ' }
-      ],
-      link: [
-        { rel: 'icon', type: 'image/png', href: '/logo-192.png' },
-        { rel: 'apple-touch-icon', href: '/logo-180.png', sizes: '180x180' },
-        { rel: 'mask-icon', href: '/logo-512.png', color: '#FFFFFF' }
-      ]
-    }
-  },
+  // app: {
+  //   head: {
+  //     meta: [
+  //       { name: 'description', content: 'Сборник текстов песен для Церкви Источник Жизни' },
+  //       { name: 'theme-color', content: '#ffffff' },
+  //       { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
+  //       { name: 'apple-mobile-web-app-capable', content: 'yes' },
+  //       { name: 'apple-mobile-web-app-title', content: 'Песни ИЖ' }
+  //     ],
+  //     link: [
+  //       { rel: 'icon', type: 'image/png', href: '/logo-192.png' },
+  //       { rel: 'apple-touch-icon', href: '/logo-180.png', sizes: '180x180' },
+  //       { rel: 'mask-icon', href: '/logo-512.png', color: '#FFFFFF' }
+  //     ]
+  //   }
+  // },
 
   postcss: {
     plugins: {
@@ -38,52 +38,56 @@ export default defineNuxtConfig({
   
   modules: [
     '@nuxtjs/device',
-    '@vite-pwa/nuxt',
+    // '@vite-pwa/nuxt',
   ],
 
-  pwa: {
-    strategies: 'generateSW', 
-    srcDir: '.',
-    client: {
-      installPrompt: true,
-      periodicSyncForUpdates: 3600,
-    },
-    manifest: {
-      "short_name": "IstokSongs",
-      "name": "IstokSongs",
-      "description": "Песни Церкви \"Источник Жизни\"",
-      "icons": [
-        {
-          "src": "/logo-192.png",
-          "sizes": "192x192",
-          "type": "image/png"
-        },
-        {
-          "src": "/logo-512.png",
-          "sizes": "512x512",
-          "type": "image/png"
-        },
-        {
-          "src": "/logo-512.png",
-          "sizes": "512x512",
-          "type": "image/png",
-          "purpose": "any"
-        },
-        {
-          "src": "/logo-512.png",
-          "sizes": "512x512",
-          "type": "image/png",
-          "purpose": "maskable"
-        },
-      ],
-      "start_url": ".",
-      "lang": "ru",
-      "display": "standalone",
-      "theme_color": '#ffffff',
-      "background_color": '#ffffff',
-      "orientation": "portrait",
-    }
+  devServer: {
+    host: '0.0.0.0',
   },
+
+  // pwa: {
+  //   strategies: 'generateSW', 
+  //   srcDir: '.',
+  //   client: {
+  //     installPrompt: true,
+    //   periodicSyncForUpdates: 3600,
+    // },
+    // manifest: {
+    //   "short_name": "IstokSongs",
+    //   "name": "IstokSongs",
+    //   "description": "Песни Церкви \"Источник Жизни\"",
+    //   "icons": [
+    //     {
+    //       "src": "/logo-192.png",
+    //       "sizes": "192x192",
+    //       "type": "image/png"
+    //     },
+    //     {
+    //       "src": "/logo-512.png",
+    //       "sizes": "512x512",
+    //       "type": "image/png"
+    //     },
+    //     {
+    //       "src": "/logo-512.png",
+    //       "sizes": "512x512",
+    //       "type": "image/png",
+    //       "purpose": "any"
+    //     },
+    //     {
+    //       "src": "/logo-512.png",
+    //       "sizes": "512x512",
+    //       "type": "image/png",
+    //       "purpose": "maskable"
+    //     },
+    //   ],
+    //   "start_url": ".",
+    //   "lang": "ru",
+    //   "display": "standalone",
+    //   "theme_color": '#ffffff',
+    //   "background_color": '#ffffff',
+    //   "orientation": "portrait",
+    // }
+  // },
 
   compatibilityDate: '2024-10-08',
 });
