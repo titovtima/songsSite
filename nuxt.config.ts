2d945ed5ab16009d1ae9560251a667a237914257
titovtima <titov.tima.spb@yandex.ru>
2026-06-17 22:42:25 +0300

    pwa caching, working offline

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import { defineNuxtConfig } from 'nuxt/config';
 
+let list = ['/', '/songs_list/1'];
+let response = await fetch('https://test.songs.titovtima.ru/api/v1/songs/info');
+// let response = await fetch('https://songs.titovtima.ru/api/v1/songs/info');
+let songsData = await response.json();
+for (let song of songsData.list) {
+  list.push('/song/' + song.id);
+}
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/main.css'],
@@ -31,56 +39,57 @@ export default defineNuxtConfig({
   
   modules: [
     '@nuxtjs/device',
-    // '@vite-pwa/nuxt',
+    '@vite-pwa/nuxt',
   ],
 
   devServer: {
     host: '0.0.0.0',
   },
 
-  // pwa: {
-  //   strategies: 'generateSW', 
-  //   srcDir: '.',
-  //   client: {
-  //     installPrompt: true,
-    //   periodicSyncForUpdates: 3600,
-    // },
-    // manifest: {
-    //   "short_name": "IstokSongs",
-    //   "name": "IstokSongs",
-    //   "description": "Песни Церкви \"Источник Жизни\"",
-    //   "icons": [
-    //     {
-    //       "src": "/logo-192.png",
-    //       "sizes": "192x192",
-    //       "type": "image/png"
-    //     },
-    //     {
-    //       "src": "/logo-512.png",
-    //       "sizes": "512x512",
-    //       "type": "image/png"
-    //     },
-    //     {
-    //       "src": "/logo-512.png",
-    //       "sizes": "512x512",
-    //       "type": "image/png",
-    //       "purpose": "any"
-    //     },
-    //     {
-    //       "src": "/logo-512.png",
-    //       "sizes": "512x512",
-    //       "type": "image/png",
-    //       "purpose": "maskable"
-    //     },
-    //   ],
-    //   "start_url": ".",
-    //   "lang": "ru",
-    //   "display": "standalone",
-    //   "theme_color": '#ffffff',
-    //   "background_color": '#ffffff',
-    //   "orientation": "portrait",
-    // }
-  // },
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: false,
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,png,svg,ico,json}'],
+      runtimeCaching: [
+        {
+          urlPattern: /\/.*\.(?:png|jpg|jpeg|svg|gif|webp|json|woff2)/,
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'static-assets',
+          }
+        },
+        {
+          urlPattern: ({ url }) => {
+            return url.pathname.includes('api');
+          },
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'api-responses',
+          }
+        },
+        // {
+        //   urlPattern: ({request}) => {
+        //     return request.method === 'GET'
+        //   },
+        //   handler: 'StaleWhileRevalidate',
+        //   options: {
+        //     cacheName: 'global-cache',
+        //     expiration: {
+        //       maxEntries: 1000,
+        //       maxAgeSeconds: 60 * 60 * 24 * 30,
+        //     }
+        //   }
+        // },
+      ]
+    }
+  },
+
+  nitro: {
+    prerender: {
+      routes: list
+    }
+  },
 
   compatibilityDate: '2024-10-08',
 });
